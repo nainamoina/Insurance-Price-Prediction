db0c6aa43ac2f0b3edd51b92b6caf6b46830a77f
nainamoina <117969458+nainamoina@users.noreply.github.com>
2022-11-11 03:08:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def main():
                 prediction = model.predict(test)
 
                 if prediction < 0:
-                    alert_message = "-."
+                    alert_message = "Invalid Data"
                 else:
                     success_message = f'Predicted Insurance Price : $ {numberFormat(prediction[0])}'
                 Insurance_Price = prediction[0]
@@ -97,9 +97,9 @@ def main():
                 db.session.add(entry)
                 db.session.commit()
             else:
-                 alert_message = "=."
+                 alert_message = "Invalid Data"
     except:
-        alert_message = ">."
+        alert_message = ""
     return render_template('Insurancepriceprediction.html',alert_message = alert_message,success_message = success_message)
 
 
